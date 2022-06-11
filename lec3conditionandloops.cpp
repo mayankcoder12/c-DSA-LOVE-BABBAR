b56e8e08716f7f665d553896c234f0aabc3be45b
mayank <mayankahuja785@gmail.com>
2022-06-11 07:34:32 +0530

    first commit

---
+++ lec3conditionandloops.cpp
@@ -0,0 +1,142 @@
+// #include <iostream>
+// using  namespace std;
+// int main(){
+    
+
+//     int  n;
+//     cin>>n;
+// cout<<"the value of n is :"<<n<<endl;
+// }
+
+
+
+// #include <iostream>
+// using  namespace std;
+// int main(){
+    
+
+//     int  a,b;
+//     cin>>a>>b;
+// cout <<"value of a and b is "<<b<<""<<a;
+
+// }
+
+
+
+// #include <iostream>
+// using  namespace std;
+// int main(){
+    
+
+//     int  a;
+//     a=cin.get();
+// cout <<"value of a "<<a<<endl;
+
+// }   // wil return aciii vaule 
+
+
+
+
+// #include <iostream>
+// using  namespace std;
+// int main(){
+
+// int a,b ;
+// cout <<"enter the value of a"<<endl;
+// cin>>a;
+// cout <<"enter the value of b"<<endl;
+// cin>>b;
+
+// if(a>b){
+//     cout <<"a is greater then b"<<endl;
+// }
+
+// if(a<b){
+//     cout <<"b is greater then a"<<endl;
+// }
+// }
+
+
+
+
+
+
+
+
+// #include <iostream>
+// using  namespace std;
+// int main(){
+
+// int a ;
+// cout <<"enter a number "<<endl;
+// cin >>a;
+// if (a>0){
+//     cout<<"number is positve";
+//     }
+
+//          else{
+//         if(a<0){
+//         cout<<"number is negative";
+//         }
+//         else{
+//             cout<<"nuber is zero"; 
+//         }
+//              }
+    
+
+// }
+
+
+
+// *******************WHILE LOOP
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+// int n;
+// cin >>n;
+// int i =1;
+// while (i<=n){
+
+//     cout<<i<<"";
+//     i=i+1;
+
+// }
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#################PATTERNS
+
+#include <iostream>
+ using namespace std;
+ int main(){
+
+
+int n;
+
+
+
+
+
+ }
