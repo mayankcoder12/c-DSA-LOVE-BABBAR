9aae0f3732240f584c424c76f59ceebcf4d000c4
mayank <mayankahuja785@gmail.com>
2022-06-19 00:58:21 +0530

    bbb

---
+++ LEC5/for loop.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+// int a=4;
+// int b=6;
+
+// cout <<"a&b"<< (a&b)<< endl;
+// cout <<"a|b"<<(a|b )<< endl;
+// cout <<"~a"<<~a << endl;
+// cout <<"a^b"<< (a^b)<< endl;
+
+
+// a&b4
+// a|b6
+// ~a-5
+// a^b2
+
+// cout <<(17>>1)<<endl;
+// cout << (17>>2)<<endl;
+// cout<<(19<<1)<<endl;
+// cout<<(21<<2)<<endl;
+
+// 8
+// 4
+// 38
+// 84
+
+
+// int i =4;
+// cout <<(i++)<<endl;
+// cout <<(++i)<<endl;
+// cout <<(i--)<<endl;
+// cout <<(--i)<<endl;
+
+// 4
+// 6
+// 6
+// 4
+
+
+
+
+// for loop
+
+// int n ;
+// cout << "enter value of n "<<endl;
+// cin>>n;
+
+// cout<<"printing couting from 1 to n"<<endl;
+// for (int i=1 ;i<=n;i++){
+// cout <<i<<endl;
+// }
+
+// int n ;
+// cout << "enter value of n "<<endl;
+// cin>>n;
+
+// cout<<"printing couting from 1 to n"<<endl;
+// int i=1;
+// for ( ;i<=n;){
+//     i++;
+// cout <<i<<endl;
+// }
+
+int n ;
+cout << "enter value of n "<<endl;
+cin>>n;
+
+cout<<"printing couting from 1 to n"<<endl;
+int i=1;
+for ( ;;){
+
+    if(i<=n){
+
+cout <<i<<endl;
+    }
+    else {
+        break;
+    }
+    i++;
+
+
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
